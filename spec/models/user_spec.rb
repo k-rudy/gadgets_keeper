2a94480c28bf0d9ee4d608195edfc26c66d5689d
Konstantin Rudy <konstantin.rudy@gmail.com>
2013-11-18 22:28:24 +0300

    Added User core validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,6 @@ describe User do
     it { should validate_presence_of :username }
     it { should ensure_length_of(:password).is_at_least(3) }
     it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:password_confirmation) }
   end
 end
